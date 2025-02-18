cf758dd524c7d1a1a9305ade933653547e590470
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2025-02-18 12:07:57 +0100

    setup iniziale

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex items-center justify-center h-screen bg-blue-100'>
+
+        <h1 className='text-3xl font-bold text-blue-700'>Favignana Booking</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
