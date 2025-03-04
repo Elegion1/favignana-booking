9f5a35c19a5423597dc5e40f6736b7e3cba93dd7
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2025-03-04 14:09:43 +0100

    Aggiunto hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
 
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Transfer from './pages/Transfer';
 import Contact from './pages/Contact';
-import TermsAndConditions from "./pages/TermsAndConditions";
+import TermsAndConditions from './pages/TermsAndConditions';
 
 function AppContent() {
-
-
   return (
-
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="transfer" element={<Transfer />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="terms-and-conditions" element={<TermsAndConditions />} />
-      </Route>
-    </Routes>
-
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="transfer" element={<Transfer />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="terms-and-conditions" element={<TermsAndConditions />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
 export default function App() {
   return (
-
-    <BrowserRouter>
-      <AppContent /> {/* Usa AppContent per forzare il re-render */}
-    </BrowserRouter>
-
+    <AppContent />
   );
 }
