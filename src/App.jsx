a05ae463fc1e70a140966ab9a49ddc5b04188358
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2025-03-06 16:43:05 +0100

    sitemap

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import './App.css';
 
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Transfer from './pages/Transfer';
 import Contact from './pages/Contact';
-import TermsAndConditions from './pages/TermsAndConditions';
+import TermsAndConditions from "./pages/TermsAndConditions";
 
-function AppContent() {
+function RedirectHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const redirectPath = searchParams.get("redirect");
+    if (redirectPath) {
+      navigate(redirectPath);
+    }
+  }, [location, navigate]);
+
+  return null;
+}
+
+function App() {
   return (
     <BrowserRouter>
+      <RedirectHandler /> {/* Gestisce il redirect */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="transfer" element={<Transfer />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="terms-and-conditions" element={<TermsAndConditions />} />
+          <Route path="/transfer" element={<Transfer />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default function App() {
-  return (
-    <AppContent />
-  );
-}
+export default App;
