32dc9372817c6d1a475f91dbfeee2493b34f0919
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2025-07-01 14:44:58 +0200

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from 'vite-plugin-vercel';
+import { getEntriesFromFs } from "vite-plugin-vercel/utils";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    vercel({
+      entries: [
+        ...(await getEntriesFromFs("endpoints/api", {
+          // Auto mapping examples:
+          //   endpoints/api/page.ts -> /api/page
+          //   endpoints/api/name/[name].ts -> /api/name/*
+          destination: "api",
+        })),
+      ],
+    }),
+  ],
   base: "/",
 });
